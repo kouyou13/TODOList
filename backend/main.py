08301068624cabcfc85ac6e78ee2df919b2cd9f9
kouyou13 <kouyou13@users.noreply.github.com>
2024-10-03 17:13:04 +0900

    保存

---
+++ backend/main.py
@@ -0,0 +1,115 @@
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+import uvicorn
+import json
+import random
+import string
+
+
+app = FastAPI()
+
+origins = [
+  'http://localhost:3000',
+  'http://localhost:3001',
+]
+
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins=origins,
+  allow_credentials=True,
+  allow_methods=['GET', 'POST', 'PUT', 'DELETE'],
+  allow_headers=['*'],
+)
+
+tasksJsonPath = './TodoList.json'
+
+class AddItem(BaseModel):
+  name: str
+  limitDate: str
+
+class UpdateItem(BaseModel):
+  id: str
+  achievement: str
+  name: str
+  limitDate: str
+
+class DeleteItem(BaseModel):
+  id: str
+
+def generateRandomString(length):
+  # 英字と数字を含む文字列を生成
+  characters = string.ascii_letters + string.digits
+  # ランダムな文字列を生成
+  randomString = ''.join(random.choice(characters) for _ in range(length))
+  randomString2 = ''.join(random.choice(characters) for _ in range(length))
+  return f'{randomString}-{randomString2}'
+
+@app.get('/')
+def read_root():
+  return 'test'
+
+@app.get('/taskList')
+def taskList():
+  try:
+    with open(tasksJsonPath, 'r') as file:
+      return json.load(file)
+  except:
+    raise HTTPException(status_code=404, detail='Item not found')
+
+@app.post('/addTask')
+def addTask(item: AddItem):
+  try:
+    try:
+      with open(tasksJsonPath, 'r') as file:
+        datas = json.load(file)
+    except:
+      datas = []
+    id = generateRandomString(10)
+    datas.insert(0, {
+      'id': id,
+      'achievement': 'Not achieved',
+      'name': item.name,
+      'limitDate': item.limitDate,
+    })
+    with open(tasksJsonPath, 'w') as file:
+      json.dump(datas, file)
+    return {'message': 'Task added successfully'}
+
+  except:
+    raise HTTPException(status_code=504, detail='Item not found')
+
+@app.post('/updateTask')
+def updateTask(item: UpdateItem):
+  # try:
+    with open(tasksJsonPath, 'r') as file:
+      datas = json.load(file)
+    for data in datas:
+      if data['id'] == item.id:
+        data['achievement'] = item.achievement
+        data['name'] = item.name
+        data['limitDate'] = item.limitDate
+        break
+    with open(tasksJsonPath, 'w') as file:
+      json.dump(datas, file)
+    return {'message': 'Task update successfully'}
+
+  # except:
+  #   raise HTTPException(status_code=504, detail='Item not found')
+
+@app.post('/deleteTask')
+def deleteTask(item: DeleteItem):
+  try:
+    with open(tasksJsonPath, 'r') as file:
+      datas = json.load(file)
+    datas = [data for data in datas if data['id'] != item.id]
+    with open(tasksJsonPath, 'w') as file:
+      json.dump(datas, file)
+    return {'message': 'Task delete successfully'}
+
+  except:
+    raise HTTPException(status_code=504, detail='Item not found')
+
+
+if __name__ == '__main__':
+  uvicorn.run(app, host='127.0.0.1', port=8000, log_level='debug')
---
+++ frontend/src/app/EditTaskModalButton.tsx
@@ -0,0 +1,163 @@
+"use client"
+import React, { useState } from "react"
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  Spacer,
+  Flex,
+  Table,
+  Tbody,
+  Tr,
+  Td,
+  Input,
+  ModalCloseButton,
+  ModalHeader,
+  ModalFooter,
+  HStack,
+  Button,
+  useToast,
+  Select,
+  useDisclosure,
+} from "@chakra-ui/react"
+import { TodoList } from "./types/types"
+import { GetToday } from "./utils/getToday"
+
+type EditTaskModalProps = {
+  isOpen: boolean
+  onClose: () => void
+  selectedTodo: TodoList
+}
+
+const _EditTaskModal = ({
+  isOpen,
+  onClose,
+  selectedTodo,
+}: EditTaskModalProps) => {
+  const toast = useToast()
+  const [selectedAchievement, setSelectedAchievement] = useState<string>(
+    selectedTodo.achievement,
+  )
+  const [inputName, setInputName] = useState(selectedTodo.name)
+  const [selectedDate, setSelectedDate] = useState(selectedTodo.limitDate)
+
+  const EditHandler = async () => {
+    try {
+      const response = await fetch("http://127.0.0.1:8000/updateTask", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          id: selectedTodo.id,
+          achievement: selectedAchievement,
+          name: inputName,
+          limitDate: selectedDate,
+        }),
+      })
+      if (!response.ok) {
+        throw new Error("Failed to update todo list")
+      }
+      toast({
+        title: "succeeded to update",
+        status: "success",
+        duration: 3000,
+        isClosable: true,
+        position: "top",
+      })
+      onClose()
+    } catch (error) {
+      console.error("Error updating todo list:", error)
+    }
+  }
+
+  return (
+    <Modal isOpen={isOpen} onClose={onClose} size="3xl">
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader textAlign={"center"}>Edit Task</ModalHeader>
+        <ModalCloseButton />
+        <Flex direction="column" rounded={6}>
+          <Table variant="none">
+            <Tbody>
+              <Tr>
+                <Td>State</Td>
+                <Td>
+                  <Select
+                    onChange={(e) => setSelectedAchievement(e.target.value)}
+                    value={selectedAchievement}
+                  >
+                    <option value="Not achieved">Not achieved</option>
+                    <option value="In progress">In progress</option>
+                    <option value="Completed">Completed</option>
+                  </Select>
+                </Td>
+              </Tr>
+              <Tr>
+                <Td>Task Name</Td>
+                <Td>
+                  <Input
+                    value={inputName}
+                    onChange={(e) => setInputName(e.target.value)}
+                  />
+                </Td>
+              </Tr>
+              <Tr>
+                <Td>Task Limit</Td>
+                <Td>
+                  <Input
+                    type="date"
+                    min={GetToday()}
+                    value={selectedDate}
+                    onChange={(e) => setSelectedDate(e.target.value)}
+                  />
+                </Td>
+              </Tr>
+            </Tbody>
+          </Table>
+        </Flex>
+        <ModalFooter>
+          <HStack w={"100%"}>
+            <Spacer />
+            <Button onClick={onClose}>Cancel</Button>
+            <Button color="white" bg="#1c1c1c" onClick={EditHandler}>
+              Save changing
+            </Button>
+          </HStack>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  )
+}
+const EditTaskModal = React.memo(_EditTaskModal)
+
+type EditTaskModalButtonProps = {
+  selectedTodo: TodoList
+  refetch: () => void
+}
+
+const _EditTaskModalButton = ({
+  selectedTodo,
+  refetch,
+}: EditTaskModalButtonProps) => {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  return (
+    <>
+      <Button bg="#1c1c1c" color="white" onClick={onOpen}>
+        Edit
+      </Button>
+      {isOpen && (
+        <EditTaskModal
+          isOpen={isOpen}
+          onClose={() => {
+            onClose()
+            refetch()
+          }}
+          selectedTodo={selectedTodo}
+        />
+      )}
+    </>
+  )
+}
+const EditTaskModalButton = React.memo(_EditTaskModalButton)
+export default EditTaskModalButton
